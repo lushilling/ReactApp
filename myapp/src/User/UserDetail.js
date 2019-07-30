8a283e1dded9f59eb2d2d68fc6f38fb3fd545f65
lushilling <lshilling@hotmail.co.uk>
2019-07-30 12:14:10 +0100

    state button working

--- myapp/src/User/UserDetail.js
+++ myapp/src/User/UserDetail.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 class UserDetail extends Component {
-    
+
+    constructor() {
+        super();
+        this.state = {
+            city: "Salford"
+        }
+        this.addClick = this.addClick.bind(this);
+    }
+
+    addClick() {
+        this.setState(
+            { city: document.getElementById("click").value }
+        )
+    }
+
     render() {
         return (
             <div>
                 <p>D.O.B.: {this.props.userData.dob} </p>
                 <p>City: {this.props.userData.city} </p>
+                <input id="click" />
+                <p>
+                    {this.state.city}
+                </p>
+                <button onClick={ this.addClick }>Change City</button>
             </div>
         );
     }
